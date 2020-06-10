edab29ca1d9cb76ee26dbb322c6a15c9c3e1fdab
shruti <vshruti@gmail.com>
2020-06-10 13:34:13 -0500

    my changes in file

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -48,7 +48,7 @@ public class SignInTest {
 
     @After
     public void tearDown() {
-        // driver.close();
+        driver.close();
     }
 
 //Test  line 14 My Account try invaild  phone#,email format 
@@ -80,7 +80,29 @@ public class SignInTest {
         //logonPassword
         WebElement passWord = driver.findElement(By.id("logonPassword"));
         passWord.clear();
-        driver.findElement(By.id("logonPassword")).sendKeys("icemoumtaim123");
+        driver.findElement(By.id("logonPassword")).sendKeys("icemountain123");
+        WebElement signINBox = driver.findElement(By.cssSelector("#LogonForm > fieldset > div:nth-child(5) > input"));
+        signINBox.click();
+
+        // JavascriptExecutor js = (JavascriptExecutor) driver;
+        // js.executeScript("window.scrollBy(0,250)", "");
+    }
+    @Test
+    public void testValidEmailInCorrectPassword() {
+        // webscraper.saksham@gmail.com
+        // icemountain123
+        driver.get(baseURL);
+        WebElement signIn = driver.findElement(By.id("header_sign_in"));
+        signIn.click();
+        WebElement emailAddress = driver.findElement(By.id("logonId"));
+        emailAddress.clear();
+        driver.findElement(By.id("logonId")).sendKeys("webscraper.saksham@gmail.com");
+        //logonPassword
+        WebElement passWord = driver.findElement(By.id("logonPassword"));
+        passWord.clear();
+        driver.findElement(By.id("logonPassword")).sendKeys("icemountain");
+        WebElement signINBox = driver.findElement(By.cssSelector("#LogonForm > fieldset > div:nth-child(5) > input"));
+        signINBox.click();
 
         // JavascriptExecutor js = (JavascriptExecutor) driver;
         // js.executeScript("window.scrollBy(0,250)", "");
