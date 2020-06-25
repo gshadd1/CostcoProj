7d6962c92cfab96345cfc8387fa480b49de6134a
kirkk-git <66497326+kirkk-git@users.noreply.github.com>
2020-06-25 05:58:47 -0500

    Update SignInTest.java

--- src/test/java/com/test/SignInTest.java
+++ src/test/java/com/test/SignInTest.java
@@ -57,7 +57,7 @@ public class SignInTest {
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         driver = new ChromeDriver();
         baseURL = "https://www.costco.com";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
     @After
