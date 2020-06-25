0b8f2e6481909a9a34a61eb70db2249aa065b314
shruti <vshruti@gmail.com>
2020-06-25 12:16:43 -0500

    my76ruyftguijkord

--- src/test/java/com/playground/ShrutiTest.java
+++ src/test/java/com/playground/ShrutiTest.java
@@ -5,6 +5,8 @@
  */
 package com.playground;
 
+import com.itexps.costco.FileUtil;
+import com.itexps.costco.LoginVO;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -28,12 +30,14 @@ public class ShrutiTest {
 
     private WebDriver driver;
     private String baseURL;
+     private static LoginVO login = null;
 
     public ShrutiTest() {
     }
 
     @BeforeClass
     public static void setUpClass() {
+         login=FileUtil.getLoginData();
     }
 
     @AfterClass
@@ -53,18 +57,37 @@ public class ShrutiTest {
         //driver.close();
     }
     
-        @Test
-    public void testAddItemNotLoggedIn15() throws Exception {
-        driver.get("https://www.costco.com/");
-        driver.findElement(By.id("Home_Ancillary_0")).click();
-        driver.findElement(By.linkText("Frito Lay Classic Mix, Variety Pack, 30-count")).click();
-        driver.findElement(By.id("add-to-cart-btn")).click();
-        driver.findElement(By.xpath("//*[@id=\"costcoModalText\"]/div[2]/div[2]/a/button")).click();
-        Thread.sleep(10000);
-        driver.findElement(By.id("shopCartCheckoutSubmitButton")).click();
-        assertEquals("Sign in to access your Costco.com account.", driver.findElement(By.xpath("//form[@id='LogonForm']/fieldset/div/span")).getText());
-    }
+//        @Test
+//    public void testAddItemNotLoggedIn15() throws Exception {
+//        driver.get("https://www.costco.com/");
+//        driver.findElement(By.id("Home_Ancillary_0")).click();
+//        driver.findElement(By.linkText("Frito Lay Classic Mix, Variety Pack, 30-count")).click();
+//        driver.findElement(By.id("add-to-cart-btn")).click();
+//        driver.findElement(By.xpath("//*[@id=\"costcoModalText\"]/div[2]/div[2]/a/button")).click();
+//        Thread.sleep(10000);
+//        driver.findElement(By.id("shopCartCheckoutSubmitButton")).click();
+//        assertEquals("Sign in to access your Costco.com account.", driver.findElement(By.xpath("//form[@id='LogonForm']/fieldset/div/span")).getText());
+//    }
 
     
+ @Test
+    public void test49ValidEmailCorrectPassword() {
+        // webscraper.saksham@gmail.com
+        // icemountain123
+        driver.get(baseURL);
+        WebElement signIn = driver.findElement(By.id("header_sign_in"));
+        signIn.click();
+        WebElement emailAddress = driver.findElement(By.id("logonId"));
+        emailAddress.clear();
+        driver.findElement(By.id("logonId")).sendKeys(login.getUsername());    ///data driven
+        //logonPassword
+        WebElement passWord = driver.findElement(By.id("logonPassword"));
+        passWord.clear();
+        driver.findElement(By.id("logonPassword")).sendKeys(login.getPassword());
+        WebElement signINBox = driver.findElement(By.cssSelector("#LogonForm > fieldset > div:nth-child(5) > input"));
+        signINBox.click();
 
+        // JavascriptExecutor js = (JavascriptExecutor) driver;
+        // js.executeScript("window.scrollBy(0,250)", "");
+    }
 }
