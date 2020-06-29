dd60910c07c32f5ebdb65f6cf338b60b2c07d802
kirkk-git <66497326+kirkk-git@users.noreply.github.com>
2020-06-28 20:44:44 -0500

    Update ManagerTest.java

--- src/test/java/com/test/ManagerTest.java
+++ src/test/java/com/test/ManagerTest.java
@@ -63,7 +63,6 @@ public class ManagerTest {
         driver = new ChromeDriver();
         baseURL = "https://www.costco.com";
         driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1600, 900));
         driver.manage().window().maximize();
     }
 
